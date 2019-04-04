2a43eb4f27c15bf797e4cd71fb7602eedeb25eec
nklttv <kolyatitov98@mail.ru>
2019-04-04 16:54:55 +0900

    comething new

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,20 @@
+echo Глобальная настйрока переменных, достаточно выполнить 1 раз
 echo 
 git config --global user.name nklttv
-git config --global user.email kolyatitov98@mail.ru
-echo 
+
+echo git config --global user.email kolyatitov98@mail.ru
+echo Добавим трекинг всех новых файлов
+
 git add -A
-echo
-git commit -a -m "commeting new"
-echo
+
+echo Коммит всех локальных изменений, не забываем в комментарии указывать суть внесенных изменений
+
+git commit -a -m "comething new"
+
+echo Объединяем все изменения из удаленного репозитория с локальной версией
+
 git pull origin master
-echo
+
+echo Применение коммита, мерж локальных измнений в удаленный репозиторий
+
 git push origin master

3380680d81040cdf4b2b986f998345d59c1433f7
nklttv <kolyatitov98@mail.ru>
2019-04-04 16:45:32 +0900

    commeting new

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+echo 
+git config --global user.name nklttv
+git config --global user.email kolyatitov98@mail.ru
+echo 
+git add -A
+echo
+git commit -a -m "commeting new"
+echo
+git pull origin master
+echo
+git push origin master
